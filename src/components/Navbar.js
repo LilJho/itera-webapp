1c35ce53872c6cf4c70453cccba4744152ba5201
LilJho <jhonnelg25@gmail.com>
2023-03-07 21:49:43 +0800

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -9,17 +9,12 @@ function Navbar() {
           IT-era
         </h2>
       </div>
-      <ul className="flex gap-8 text-2xl font-medium leading-none cursor-pointer">
+      <ul className="flex gap-8 p-6 text-2xl font-medium leading-none bg-white bg-opacity-50 border border-gray-300 rounded-lg shadow-lg cursor-pointer backdrop-filter backdrop-blur-lg">
         <li className="link-underline">About us</li>
         <li className="link-underline">Services</li>
         <li className="link-underline">Clients</li>
         <li className="link-underline">Projects</li>
       </ul>
-      <div>
-        <button className="p-2 font-medium bg-[#FABC16] rounded-xl text-[#808080]">
-          Inquire Now!
-        </button>
-      </div>
     </nav>
   );
 }
