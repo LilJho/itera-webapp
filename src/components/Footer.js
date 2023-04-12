aadb86809b0ba138a4466a53f9e20ab8a010a0b8
LilJho <jhonnelg25@gmail.com>
2023-04-12 21:57:08 +0800

    added footer

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="flex items-center justify-center p-4 mt-6 bg-gray-800">
+      <p className="text-white">All Rights reserved - made by Jhonnel Garcia</p>
+    </div>
+  );
+};
+
+export default Footer;
