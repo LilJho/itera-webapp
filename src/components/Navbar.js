352dc469cfa86e7c66b792902697efc52c1fbea3
LilJho <jhonnelg25@gmail.com>
2023-04-12 13:16:40 +0800

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,13 +3,11 @@ import Image from "next/image";
 
 function Navbar() {
   return (
-    <nav className="flex items-end justify-between p-4 font-NotoSans">
-      <div>
-        <h2 className="text-4xl font-extrabold leading-none cursor-pointer">
-          IT-era
-        </h2>
-      </div>
-      <ul className="flex gap-8 p-6 text-2xl font-medium leading-none bg-white bg-opacity-50 border border-gray-300 rounded-lg shadow-lg cursor-pointer backdrop-filter backdrop-blur-lg">
+    <nav className="flex items-center justify-between p-4 font-NotoSans">
+      <h2 className="text-2xl font-extrabold leading-none cursor-pointer">
+        IT-era
+      </h2>
+      <ul className="flex gap-8 p-6 text-xs font-medium leading-none bg-white bg-opacity-50 border border-gray-300 rounded-lg shadow-lg cursor-pointer backdrop-filter backdrop-blur-lg">
         <li className="link-underline">About us</li>
         <li className="link-underline">Services</li>
         <li className="link-underline">Clients</li>
