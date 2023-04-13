e3f6a6806c152b2c34783ceaaae2635e54bafd2c
LilJho <jhonnelg25@gmail.com>
2023-04-13 11:14:30 +0800

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -3,7 +3,7 @@ import React from "react";
 
 const About = () => {
   return (
-    <div className="flex flex-col items-center justify-center w-full p-4 bg-gray-300">
+    <div className="flex flex-col items-center justify-center w-full py-[10rem] bg-gray-100">
       <h4 className="text-xl font-semibold font-NotoSan">About us</h4>
       <article className="flex flex-col justify-center gap-[8rem] items center">
         <div className="flex items-center justify-center gap-4 mt-8">
@@ -82,26 +82,33 @@ const About = () => {
         <div className="flex items-center justify-center gap-4 mt-8">
           <Image
             src={"/about/andrea.jpg"}
-            width={300}
+            width={400}
             height={300}
             className="rounded-xl"
           />
-          <p className="text-xs text-gray-600 max-w-[20rem]">
-            The startup software firm is a cutting-edge technology company that
-            specializes in creating innovative projects using the best practices
-            in the software field. With a team of highly skilled software
-            engineers and developers, the company is committed to delivering
-            world-class software solutions that meet the highest quality
-            standards. From design and development to testing and deployment,
-            the company uses industry best practices to ensure that their
-            projects are scalable, reliable, and secure. The firm is focused on
-            leveraging the latest technologies and tools to provide their
-            clients with cutting-edge software that is tailored to their
-            specific needs. With a strong emphasis on collaboration and
-            communication, the company is able to work closely with their
-            clients to deliver projects that exceed their expectations and drive
-            business growth.
-          </p>
+          <div className="flex flex-col justify-between min-h-[426px]">
+            <p className="text-xs text-gray-600 max-w-[20rem] ">
+              <span className="text-base font-semibold">
+                The startup software firm{" "}
+              </span>
+              is a cutting-edge technology company that specializes in creating
+              innovative projects using the best practices in the software
+              field. With a team of highly skilled software engineers and
+              developers, the company is committed to delivering world-class
+              software solutions that meet the highest quality standards. From
+              design and development to testing and deployment, the company uses
+              industry best practices to ensure that their projects are
+              scalable, reliable, and secure. The firm is focused on leveraging
+              the latest technologies and tools to provide their clients with
+              cutting-edge software that is tailored to their specific needs.
+              With a strong emphasis on collaboration and communication, the
+              company is able to work closely with their clients to deliver
+              projects that exceed their expectations and drive business growth.
+            </p>
+            <button className="p-2 text-white bg-teal-500 rounded">
+              Work with us!
+            </button>
+          </div>
         </div>
       </article>
     </div>
