00feba2e449d2aff55bb723392c367b992e799e4
LilJho <jhonnelg25@gmail.com>
2023-04-16 16:39:33 +0800

    Update Projectsv2.js

--- src/components/Projectsv2.js
+++ src/components/Projectsv2.js
@@ -10,7 +10,7 @@ import "swiper/css/scrollbar";
 
 const Projectsv2 = () => {
   return (
-    <div className="flex flex-col items-center mt-[10rem] mb-[5rem]">
+    <div className="flex flex-col items-center mt-2 mb-[5rem]">
       <h1 className="mt-[5rem] text-xl font-semibold font-NotoSans">
         Our Latest Works
       </h1>
@@ -25,7 +25,7 @@ const Projectsv2 = () => {
         navigation
         pagination={{ clickable: true }}
         scrollbar={{ draggable: true }}
-        className="w-full md:w-[25rem] h-[600px]"
+        className="w-full md:w-[25rem]"
       >
         <SwiperSlide>{projects()}</SwiperSlide>
         <SwiperSlide>{projects()}</SwiperSlide>
@@ -38,31 +38,14 @@ const Projectsv2 = () => {
 
 const projects = () => {
   return (
-    <div className="mx-auto flex w-[400px] h-[500px] md:max-w-[510px] md:max-h-[533px] flex-col mt-8 overflow-hidden bg-white shadow-lg rounded-2xl backdrop-filter backdrop-blur-lg">
-      <div className="flex items-center justify-center h-full p-4 bg-[url('/project-bg/bgnew.svg')] bg-cover bg-center">
+    <div className="flex items-center justify-center w-full h-full ">
+      <div className="h-[12rem] w-[20rem]  overflow-hidden bg-white shadow-lg rounded-2xl backdrop-filter backdrop-blur-lg">
         <Image
           src={"/projects/psu.svg"}
-          width={470}
-          height={470}
+          fill
           className="rounded-2xl"
           alt="PSU webApp"
         />
-      </div>
-      <div className="flex flex-col items-center justify-center p-4 w-[400px] md:w-[510px]">
-        <div className="flex flex-col items-center justify-center w-full">
-          <h4 className="text-base font-medium font-NotoSans">
-            PSU web application
-          </h4>
-          <p className="text-xs text-gray-600 font-WorkSans">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores
-            nam molestias temporibus odit porro, pariatur natus doloremque
-            accusantium cum vero, velit sed atque dicta eveniet similique!
-            Delectus consectetur ipsum quidem.
-          </p>
-          <button className="p-2 text-xs text-white bg-yellow-500 rounded-lg mt-[3rem] font-NotoSans">
-            View Project
-          </button>
-        </div>
       </div>
     </div>
   );
